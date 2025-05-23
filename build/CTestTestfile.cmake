ccf65454620e1ba871ad7cda8a073a081a8e0bae
KP <rinyakok@gmail.com>
2025-05-23 21:18:51 +0200

    Add compiler & linker options to CMake configuration. Add option support for both MSVC & GNU compiler Introduce Release & Debug configuration

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(IntegratorUnitTests "D:/Workset/polynomial_integral/build/IntegratorUnitTests.exe")
-set_tests_properties(IntegratorUnitTests PROPERTIES  _BACKTRACE_TRIPLES "D:/Workset/polynomial_integral/CMakeLists.txt;51;add_test;D:/Workset/polynomial_integral/CMakeLists.txt;0;")
+set_tests_properties(IntegratorUnitTests PROPERTIES  _BACKTRACE_TRIPLES "D:/Workset/polynomial_integral/CMakeLists.txt;91;add_test;D:/Workset/polynomial_integral/CMakeLists.txt;0;")
 subdirs("_deps/googletest-build")
